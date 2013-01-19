43ea33cec9ead31b704323a8e3a99572fabd33e6
Saber <sabersalv@gmail.com>
2013-01-19 10:57:22 +0800

    finalize build.sh

--- build.sh
+++ build.sh
@@ -10,48 +10,47 @@
 #
 # Example:
 #
-#   ./build.sh linux/amd64
-#   ./build.sh -d /usr/share/freedom-routes linux/amd64
+#   ./build.sh -d /usr/share/freedom-routes    # build with system go
+#   ./build.sh release                         # build $RELEASEs with user complied go and upload the package to s3.
 
 VERSION=$(sed -rn 's/.*const VERSION.*"([0-9.]+)".*/\1/p' main.go)
 FILES="routes/templates freedom-routes.etc README.md CHANGELOG.md"
-#RELEASE="homebrew/amd64 homebrew/386 windows/386 windows/amd64"
-RELEASE="homebrew/amd64 homebrew/386"
-
+RELEASE="homebrew/amd64 homebrew/386 windows/386 windows/amd64"
 declare -A OS_MAP=(
 	[homebrew]="darwin"
 )
-
 declare -A DIR_MAP=(
 	[homebrew]="/usr/share/freedom-routes"
 )
 
-function cgo-enabled { [[ "$1" == "$GOHOSTOS" ]] && echo 1 || echo 0; }
-function ext { [[ $1 == "windows" ]] && echo .exe || echo ""; }
+platform=""
+os=""
+arch=""
+assets_dir=""
 
-# dist(platform, os, arch, assets_dir)
+function cgo_enabled { [ "$1" == "$GOHOSTOS" ] && echo 1 || echo 0; }
+function ext { [ $1 == "windows" ] && echo .exe || echo ""; }
+
+# dist{platform, os, arch, assets_dir}
 function dist {
 	rm -r dist >/dev/null
 	mkdir dist
 
 	cp -r $FILES dist
-	build $*
+	build
 }
 
-# build(platform, os, arch, assets_dir)
+# build{platform, os, arch, assets_dir}
 function build {
-	platform=$1; os=$2; arch=$3; assets_dir=$4
 	echo -e "\nbuilding $platform/$arch"
 
 	sed -i "/const ASSETS_MODE/s~.*~const ASSETS_MODE = \"$assets_dir\"~" routes/routes.go
-	CGO_ENABLED=$(cgo-enabled $os) GOOS=$os GOARCH=$arch $GOROOT/bin/go build -o "dist/freedom-routes$(ext $os)"
+	CGO_ENABLED=$(cgo_enabled $os) GOOS=$os GOARCH=$arch $GOROOT/bin/go build -o "dist/freedom-routes$(ext $os)"
 	sed -i '/const ASSETS_MODE/s/.*/const ASSETS_MODE = "source"/' routes/routes.go
 }
 
-# package(platform, os, arch)
+# package{platform, os, arch}
 function package {
-	platform=$1; os=$2; arch=$3;
-	
 	echo "packing $platform/$arch" 
 	mkdir dist/freedom-routes-$VERSION
 	cp -r dist/* dist/freedom-routes-$VERSION 2>/dev/null
@@ -75,35 +74,35 @@ function upload {
 }
 
 #
-# main
+# ¤main
 # ----
 
 eval "$(go env)"
-ASSETS_DIR="runtime"
-is_package=false
 
+o_assets_dir="runtime"
+o_package=false
 while getopts "d:p" opt; do
   case $opt in
-    d ) ASSETS_DIR=$OPTARG ;;
-		p ) is_package=true
+    d ) o_assets_dir=$OPTARG ;;
+		p ) o_package=true ;;
   esac
 done
-
-shift $(( OPTIND-1 ))
+shift $(( OPTIND - 1 ))
 
 case $1 in
 	"" )
-		dist $GOOS $GOOS $GOARCH $ASSETS_DIR
-		[[ $is_package == true ]] && package $GOOS $GOOS $GOARCH
+		platform=$GOOS; os=$GOOS; arch=$GOARCH; assets_dir=$o_assets_dir
+		dist
+		[ $o_package == true ] && package
 		;;
 	release )
 		GOROOT="/home/guten/dev/src/go/go"
 		rm *.zip *.tar.gz 2>/dev/null
 		for release in $RELEASE; do
 			platform=${release%/*}; arch=${release#*/}; os=${OS_MAP[$platform]-$platform}
-			assets_dir=${DIR_MAP[$platform]-$ASSETS_DIR}
-			dist $platform $os $arch $assets_dir
-			package $platform $os $arch
+			assets_dir=${DIR_MAP[$platform]-$o_assets_dir}
+			dist
+			package
 		done
 		upload *.zip *.tar.gz
 		;;
@@ -114,8 +113,8 @@ case $1 in
 	* )
 		GOROOT="/home/guten/dev/src/go/go"
 		platform=${1%/*}; arch=${1#*/}; os=${OS_MAP[$platform]-$platform}
-		dist $platform $os $arch $ASSETS_DIR
-		[[ $is_package == true ]] && package $platform $os $arch
+		dist
+		[ $o_package == true ] && package
 		;;
 esac
 
