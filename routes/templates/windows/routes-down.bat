31dd5227c148dfd5850fb8630f971186cb6530a3
zhuzeitou <zhuzeitou@gmail.com>
2017-09-21 13:58:53 +0800

    fix windows routes-down

--- routes/templates/windows/routes-down.bat
+++ routes/templates/windows/routes-down.bat
@@ -7,7 +7,7 @@ if not %errorlevel% == 0 (
   exit 1
 )
 
-cd %~dp0
+cd /d %~dp0
 echo ÒÆ³ýÂ·ÓÉ±í...
 rundll32.exe cmroute.dll,SetRoutes /STATIC_FILE_NAME del.txt /DONT_REQUIRE_URL /IPHLPAPI_ACCESS_DENIED_OK
 pause
